89e8c00a1b0256ea70f2b25da9f440a275a5aac0
Elvir Suroji <esuroji@gmail.com>
2024-12-17 16:57:11 +0100

    [red] returns Buzz for multiples of five

--- src/1_1_Fizzbuzz/index.spec.ts
+++ src/1_1_Fizzbuzz/index.spec.ts
@@ -8,4 +8,8 @@ describe('fizzbuzz', () => {
     it('should be able to return "Fizz" for multiples of three', () => {
         expect(fizzbuzz(3)).toBe('Fizz')
     })
+
+    it('should be able to return "Buzz" for multiples of five', () => {
+        expect(fizzbuzz(5)).toBe('Buzz')
+    })
 })
