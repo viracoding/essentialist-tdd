8740b6aa67836ccce71e2117ac2cd29dd163ad41
Elvir Suroji <esuroji@gmail.com>
2024-12-17 16:57:43 +0100

    [green] returns Buzz for multiples of five

--- src/1_1_Fizzbuzz/index.ts
+++ src/1_1_Fizzbuzz/index.ts
@@ -2,5 +2,8 @@ export function fizzbuzz(num: number): string {
     if (num % 3 === 0) {
         return "Fizz"
     }
+    if (num % 5 === 0) {
+        return "Buzz"
+    }
     return num.toString();
 }
