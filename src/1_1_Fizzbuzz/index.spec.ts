d59c16bffb04a4445f22f9313fd39119ab1a7535
Elvir Suroji <esuroji@gmail.com>
2024-12-17 16:55:40 +0100

    [red] returns Fizz for multiples of three

--- src/1_1_Fizzbuzz/index.spec.ts
+++ src/1_1_Fizzbuzz/index.spec.ts
@@ -4,4 +4,8 @@ describe('fizzbuzz', () => {
     it('returns string', () => {
         expect(typeof fizzbuzz(2)).toBe('string')
     })
+
+    it('should be able to return "Fizz" for multiples of three', () => {
+        expect(fizzbuzz(3)).toBe('Fizz')
+    })
 })
