f30c8c79423b66604f9bcd6856b2200d1f5893a3
Elvir Suroji <esuroji@gmail.com>
2025-01-16 14:08:50 +0100

    some modifications

--- src/2_2_Stats_Calculator/src/index.spec.ts
+++ src/2_2_Stats_Calculator/src/index.spec.ts
@@ -4,19 +4,19 @@ describe('stats calculator', () => {
     const inputNumbers = [2, 4, 21, -8, 53, 40];
     describe(`for the following sequence of integer numbers [${inputNumbers}]`, () => {
         const result = calculateStats(inputNumbers)
-        it('returns -8 as the minimum value', () => {
+        it('should return -8 as the minimum value', () => {
             expect(result.minimum).toBe(-8)
         })
 
-        it('returns 53 as the maximum value', () => {
+        it('should return 53 as the maximum value', () => {
             expect(result.maximum).toBe(53)
         })
 
-        it('returns 6 as the elements count', () => {
+        it('should return 6 as the elements count', () => {
             expect(result.elementsCount).toBe(6)
         })
 
-        it('returns 18.67 as the average', () => {
+        it('should return 18.67 as the average', () => {
             expect(result.average).toBe(18.67)
         })
     })
