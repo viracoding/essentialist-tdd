aca7143d64e76dfed735d3a19860891464ee9714
Elvir Suroji <esuroji@gmail.com>
2025-01-20 15:03:26 +0100

    [new] evaluates value TRUE

--- src/3_2_Boolean_Calculator/src/index.spec.ts
+++ src/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,9 @@
+import { BooleanCalculator } from "./index";
 
 describe('boolean calculator', () => {
-
+    describe('evaluates single values', () => {
+        it('TRUE', () => {
+            expect(BooleanCalculator.evaluate("TRUE")).toBeTruthy()
+        })
+    })
 })
