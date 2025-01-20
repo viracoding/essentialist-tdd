2e57493a917d41c4804aa5ab812ad435c6f66d9e
Elvir Suroji <esuroji@gmail.com>
2025-01-20 20:11:23 +0100

    [refactor] and or

--- src/3_2_Boolean_Calculator/src/index.ts
+++ src/3_2_Boolean_Calculator/src/index.ts
@@ -8,19 +8,24 @@ export class BooleanCalculator {
         }
         return input === "TRUE"
     }
-
     private static returnInverse(input: string) {
         return !this.isTrue(input)
+    }
+    private static and(left: boolean, right: boolean) {
+        return left && right
+    }
+    private static or(left: boolean, right: boolean) {
+        return left || right
     }
 
     public static evaluate(expression: string): boolean | undefined {
         const expressionParts = expression.split(" ")
-        if (expression.split(" ").length === 3) {
-            if (expression.split(" ")[1] === "AND") {
-                return expression.split(" ")[0] === "TRUE" && expression.split(" ")[2] === "TRUE";
+        if (expressionParts.length === 3) {
+            if (expressionParts[1] === "AND") {
+                return this.and(this.isTrue(expressionParts[0]), this.isTrue(expressionParts[2]));
             }
-            if (expression.split(" ")[1] === "OR") {
-                return expression.split(" ")[0] === "TRUE" || expression.split(" ")[2] === "TRUE";
+            if (expressionParts[1] === "OR") {
+                return this.or(this.isTrue(expressionParts[0]), this.isTrue(expressionParts[2]));
             }
         }
         if (expressionParts.length === 2 && expressionParts[0] === "NOT") {
